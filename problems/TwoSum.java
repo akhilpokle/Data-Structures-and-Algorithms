4f9af3aeb7011a71ea3171e0f57a7cb36b036f8c
AKHIL POKLE <akhilpokle96@gmail.com>
2020-03-19 12:51:26 +0530

    Update TwoSum.java

--- problems/TwoSum.java
+++ problems/TwoSum.java
@@ -19,7 +19,7 @@ public class Solution {
   //brute force O(n^2) time O(1) space
   public int[] twoSum(int[] nums,int target){
     for(int i=0;i<nums.length-1;i++){
-      for(int j=1;j<nums.length;j++){
+      for(int j=i;j<nums.length;j++){
         if(nums[i]+nums[j] == target) return new int[]{i,j};
       }
     }
@@ -28,15 +28,47 @@ public class Solution {
 
   //Sorting O(nlogn) time O(1) space
   public int[] twoSum(int[] nums,int target){
-    int left = 0;
-    int right = nums.length-1;
-    Arrays.sort(nums);
-    while(left<right){
-      if(nums[left]+nums[right] == target) return new int[]{i,j};
-      else if(nums[left]+nums[right] > target) right--;
-      else start++;
-    }
-    return new int[]{-1,-1};
+    int[] nums2 = Arrays.copyOf(nums, nums.length);
+	    	Arrays.sort(nums2);
+	    	int a = 0, b = 0;
+	    	int start = 0, end = nums2.length-1;
+	    	//find two nums
+	    	while(start<end){
+	    		int sum = nums2[start] + nums2[end];
+	    		if(sum < target)
+	    			start++;
+	    		else if(sum > target)
+	    			end--;
+	    		else{
+	    			a = nums2[start]; b = nums2[end];
+	    			break;
+	    		}
+	    	}
+	    	//find the index of two numbers
+	    	int[] res = new int[2];
+	    	for(int i = 0; i < nums.length; i++){
+	    		if(nums[i] == a){
+	    			res[0] = i;
+	    			break;
+	    		}
+	    	}
+	    	if(a != b){
+	    		for(int i = 0; i < nums.length; i++){
+		    		if(nums[i] == b){
+		    			res[1] = i;
+		    			break;
+		    		}
+		    	}
+	    	} else{
+	    		for(int i = 0; i < nums.length; i++){
+		    		if(nums[i] == b && i != res[0]){
+		    			res[1] = i;
+		    			break;
+		    		}
+		    	}
+	    	}
+	    	
+	    	return res;
   }
   
   
