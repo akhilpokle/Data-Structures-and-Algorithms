9980a13d3e906ff228fcee4349fe2c786bf3553e
AKHIL POKLE <akhilpokle96@gmail.com>
2020-05-06 22:24:01 +0530

    Create majorityElement.java

---
+++ problems/majorityElement.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int majorityElement(int[] num) {
+
+        int major=num[0], count = 1;
+        for(int i=1; i<num.length;i++){
+            if(count==0){
+                count++;
+                major=num[i];
+            }else if(major==num[i]){
+                count++;
+            }else count--;
+            
+        }
+        return major;
+    }
+}
