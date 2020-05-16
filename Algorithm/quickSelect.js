7f846cd95142e1c80c1edf8a5ba52919992620c6
AKHIL POKLE <akhilpokle96@gmail.com>
2020-05-16 16:21:05 +0530

    Update quickSelect.js

--- Algorithm/quickSelect.js
+++ Algorithm/quickSelect.js
@@ -1,34 +1,35 @@
-let arr = [0,1,2,3,0];
+    var findKthLargest = function(nums, k) {
+        return quickSelect(nums,nums.length-k,0,nums.length-1);
+    };
 
-function partition(arr,low,high){
-  let pivot = arr[high];
-  let idx = low;
-  for(let i=low;i<high;i++){
-    if(arr[i]<=pivot){
-      let temp = arr[i];
-      arr[i] = arr[idx];
-      arr[idx] = temp;
-      idx++;
+    function quickSelect(nums,k,low,high){
+        if(low == high){
+            return nums[low];
+        }
+        let pivot = partition(nums,low,high);
+        if(pivot == k) return nums[pivot];
+        if(pivot<k){
+            return quickSelect(nums,k,pivot+1,high);
+        }else{
+            return quickSelect(nums,k,low,pivot-1);
+        }
     }
-  }
-  let temp = arr[idx];
-  arr[idx] = arr[high];
-  arr[high] = temp;
-  return idx;
-}
 
-function quickSelect(arr,low,high,k){
-  if(low>high) return;
-  let part = partition(arr,low,high);
-  if(part+1 == k){
-    return part;
-  }
-  if(part<k){ 
-    return quickSelect(arr,part+1,high,k);
-  }else{
-    return quickSelect(arr,low,part-1,k);
-  }
-}
-
-let res = quickSelect(arr,0,arr.length-1,4);
-console.log(arr[res]);
+    function partition(nums,low,high){
+        let idx = Math.floor(low+(high-low)/2);
+        swap(nums,low,idx);
+        let pivot = low;
+        low++;
+        while(low<=high){
+            while(low<=high && nums[low] < nums[pivot])
+                low++;
+            while(low<=high && nums[high] >= nums[pivot])
+                high--;
+            if(low>high){
+                swap(nums,pivot,high);
+                return high;
+            }
+            swap(nums,low,high);
+        }
+        return high;
+    }
