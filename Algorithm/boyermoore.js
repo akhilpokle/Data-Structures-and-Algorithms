e67df7328157b8820b808dbce777d3031f4ae266
AKHIL POKLE <akhilpokle96@gmail.com>
2020-07-04 20:10:58 +0530

    Create boyermoore.js

---
+++ Algorithm/boyermoore.js
@@ -0,0 +1,34 @@
+let string = "ATAATTACCAACATCATAATTACCAACATCATAATTACCAACATCATAATTACCAACATCATC";
+let pattern = "ATC";
+
+let M = pattern.length;
+let N = string.length;
+let skip;
+let res = [];
+
+let map = new Array(256);
+
+for(let c = 0;c<256;c++){
+  map[c] = -1;
+}
+
+for(let j=0;j<M;j++){
+  map[pattern[j]] = j;
+}
+
+for(let i=0;i<=N-M;i+=skip){
+  skip=0;
+  for(let j = M-1;j>=0;j--){
+
+    if(pattern[j] != string[i+j]){
+      skip = Math.max(1,j-map[string[i+j]]);
+      break;
+    }
+  }
+  if(skip == 0){
+    res.push(i);
+    skip++;
+  }
+}
+
+console.log(res);
